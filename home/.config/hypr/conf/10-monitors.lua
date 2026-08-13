7dbd620c422436641784ccd06bf7d31c1bbdbb1c
Remenod <remenod@gmail.com>
2026-08-13 19:38:39 +0300

    bring back my 240hz

--- home/.config/hypr/conf/10-monitors.lua
+++ home/.config/hypr/conf/10-monitors.lua
@@ -1,5 +1,5 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
-hl.monitor({ output = "eDP-1", mode = "1920x1080@60", position = "0x0", scale = 1.00, transform = 0 })
+hl.monitor({ output = "eDP-1", mode = "1920x1080@240", position = "0x0", scale = 1.00, transform = 0 })
 hl.monitor({ output = "desc:LG Electronics 19EN33 307NDZJJW461", mode = "1366x768@77", position = "-1300x-1152", scale = 0.67 })
 hl.monitor({ output = "desc:RGT 0x1352 0x01010101", mode = "1366x768@77", position = "-1300x-1152", scale = 0.67 })
 hl.monitor({ output = "HEADLESS-1", mode = "1920x1080@60", position = "auto-right", scale = 1 })
